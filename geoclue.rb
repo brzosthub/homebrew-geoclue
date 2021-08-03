5996a241b844639118cdd3a0b1bba6fc162e3da6
Hubert Brzostowski <hubertbrzostowski@Huberts-MacBook-Pro.local>
2021-08-03 23:41:58 +0200

    More..

---
+++ geoclue.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Geoclue < Formula
+  desc "GeoClue location framework"
+  homepage ""
+  url "https://github.com/freedesktop/geoclue/archive/refs/tags/0.12.99.tar.gz"
+  sha256 "ee4707ebb122b77093ccc08b51faf3af91f270258be1352a3222b5a21c81b241"
+  license "NOASSERTION"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test geoclue`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
